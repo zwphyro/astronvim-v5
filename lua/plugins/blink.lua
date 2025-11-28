d8d361e072bd3a860ceeb4be1a671c8c19a61edd
zwphyro <tofumaks@gmail.com>
2025-11-28 05:02:15 +0300

    update lock

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,8 +5,8 @@ return {
       preset = "default",
       ["<Tab>"] = {
         function()
-          local status, vtext = pcall(require, "codeium.virtual_text")
-          if status and vtext.get_current_completion_item() then
+          local ok, vtext = pcall(require, "codeium.virtual_text")
+          if ok and vtext.get_current_completion_item() then
             vim.api.nvim_input(vtext.accept())
             return true
           else
